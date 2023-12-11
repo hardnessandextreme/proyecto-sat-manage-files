81c6252904c18ae12a08d52c0c580fddd7886779
Jonathan Quinté <86927718+hardnessandextreme@users.noreply.github.com>
2023-12-11 17:41:51 -0500

    Refactor file creation and add folder verification

--- funcionalidades/funcs.py
+++ funcionalidades/funcs.py
@@ -1,16 +1,31 @@
 import os
+import os
 
 def limpiar_pantalla():
     os.system('cls')
 
-def crear_archivos():
-    usuarios = open("datos/usuarios.txt", 'a', encoding='utf-8')
-    usuarios.close()
-    materias = open("datos/materias.txt", 'a', encoding='utf-8')
-    materias.close()
-    docentes = open("datos/docentes.txt", 'a', encoding='utf-8')
-    docentes.close()
-    matriculas = open("datos/matriculas.txt", 'a', encoding='utf-8')
-    matriculas.close()
-    notas = open("datos/notas.txt", 'a', encoding='utf-8')
-    notas.close()
+# def crear_archivos():
+#     usuarios = open("datos/usuarios.txt", 'a', encoding='utf-8')
+#     usuarios.close()
+#     materias = open("datos/materias.txt", 'a', encoding='utf-8')
+#     materias.close()
+#     docentes = open("datos/docentes.txt", 'a', encoding='utf-8')
+#     docentes.close()
+#     matriculas = open("datos/matriculas.txt", 'a', encoding='utf-8')
+#     matriculas.close()
+#     notas = open("datos/notas.txt", 'a', encoding='utf-8')
+#     notas.close()
+
+def verificar_carpeta_datos():
+    if not os.path.exists("datos"):
+        os.makedirs("datos")
+        usuarios = open("datos/usuarios.txt", 'a', encoding='utf-8')
+        usuarios.close()
+        materias = open("datos/materias.txt", 'a', encoding='utf-8')
+        materias.close()
+        docentes = open("datos/docentes.txt", 'a', encoding='utf-8')
+        docentes.close()
+        matriculas = open("datos/matriculas.txt", 'a', encoding='utf-8')
+        matriculas.close()
+        notas = open("datos/notas.txt", 'a', encoding='utf-8')
+        notas.close()
