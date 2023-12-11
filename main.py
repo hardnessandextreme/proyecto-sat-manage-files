4251e5ee3e057fd18897c483ff5f24b3ecf6378f
Jonathan Quinté <86927718+hardnessandextreme@users.noreply.github.com>
2023-12-11 01:55:21 -0500

    Refactor menu_principal function

--- main.py
+++ main.py
@@ -1,38 +1,35 @@
 from menu.m_mantenimiento import menu_de_mantenimiento
 from menu.m_matriculacion import menu_matriculacion
 from menu.m_registronota import menu_registro_nota
-from funcionalidades.funcs import limpiar_pantalla
-
+from funcionalidades.funcs import *
 
 def menu_principal():
     ciclo= True
     while ciclo:
         limpiar_pantalla()
-        print("Menu Principal")
+        print("Menu Principal\n")
         print("1. Mantenimiento\n"
               "2. Matriculacion\n"
               "3. Registro de notas\n"
-              "4. Salir")
+              "4. Salir\n")
         
         try:
+            crear_archivos()
             opcion=int(input("Ingresa la opcion: "))
 
             if opcion == 1:
-                print("Estoy en matentenimiento")
                 menu_de_mantenimiento()
             elif opcion == 2:
-                print("Estoy en matriculacion")
                 menu_matriculacion()
             elif opcion == 3:
-                print("Estoy en Registro de notas")
                 menu_registro_nota()
             elif opcion == 4:
                 ciclo= False
-            else:
-                print('Ingrese una opción válida')
 
         except Exception as e:
-            pass
+            print(f"\nError en el menu principal: {e}")
+            input("\nPresione una tecla para continuar...")
+            ciclo = False
 
 if __name__=="__main__":
     menu_principal()
