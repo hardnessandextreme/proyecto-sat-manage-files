beac99513a14d63832bcf5690da204ff621fc06d
Jonathan Quinté <86927718+hardnessandextreme@users.noreply.github.com>
2023-12-11 01:55:27 -0500

    Add file creation functionality to funcs.py

--- funcionalidades/funcs.py
+++ funcionalidades/funcs.py
@@ -2,3 +2,15 @@ import os
 
 def limpiar_pantalla():
     os.system('cls')
+
+def crear_archivos():
+    usuarios = open("datos/usuarios.txt", 'a', encoding='utf-8')
+    usuarios.close()
+    materias = open("datos/materias.txt", 'a', encoding='utf-8')
+    materias.close()
+    docentes = open("datos/docentes.txt", 'a', encoding='utf-8')
+    docentes.close()
+    matriculas = open("datos/matriculas.txt", 'a', encoding='utf-8')
+    matriculas.close()
+    notas = open("datos/notas.txt", 'a', encoding='utf-8')
+    notas.close()
